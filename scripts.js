b9474b62e294e057cc2f38014dd4f97b6f71f7ae
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-26 15:47:55 -0700

    Get latitude and store in lat variable

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       lon = position.coords.longitude;
-
+      lat = position.coords.latitude;
     }); // END GET CURRENT POSITION
   } else {
     alert("Error: geolocation is not supported.");
