cb23984c9e7f2970985fc45e0b315c658ed87526
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-26 15:45:34 -0700

    Call getCurrentPosition method if geolocation is supported

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+    }); // END GET CURRENT POSITION
   } else {
     alert("Error: geolocation is not supported.");
   } // END NAVIGATOR.GEOLOCATION
