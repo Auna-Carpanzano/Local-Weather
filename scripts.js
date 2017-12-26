435d51bb24f8e2100c3c195afa17c501e288f844
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-26 15:49:17 -0700

    Concatenate api url with longitude and latitude coordinates

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     navigator.geolocation.getCurrentPosition(function(position) {
       lon = position.coords.longitude;
       lat = position.coords.latitude;
+      var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + lon + '';
     }); // END GET CURRENT POSITION
   } else {
     alert("Error: geolocation is not supported.");
